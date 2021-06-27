cdc2b356248a9869a260a552e4c829c6171063dd
mandeep <singh.mr.1996@gmail.com>
2021-06-27 15:58:44 +0530

    Initialize Setup For Line Comparision Problems Java

---
+++ src/com/bridgelabz/LineCoparisonProblem.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class LineCoparisonProblem 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcom To Line Comparison Problems Java");
+	}
+
+}
