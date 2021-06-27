7b0aa2dd5b5fce8e64bf2cb64e79f5f3dffbb195
mandeep <singh.mr.1996@gmail.com>
2021-06-27 16:07:30 +0530

    Calculate Two Line Are Equal Or Not

--- src/com/bridgelabz/LineCoparisonProblem.java
+++ src/com/bridgelabz/LineCoparisonProblem.java
@@ -5,14 +5,20 @@ public class LineCoparisonProblem
 
 	public static void main(String[] args) 
 	{
-		System.out.println("Welcom To Line Comparison Problems Java");
-		int x1=4;
-		int x2=8;
-		int y1=3;
-		int y2=6;
-		System.out.println("Length at 2 Points::" +"("+x1+","+y1+")"+"and"+"("+x2+","+y2+")");
-		double Length_line=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))); 
-		System.out.println("Length of a line::"+Length_line);
+		int x1=1;
+		int y1=1;
+		int x2=5;
+		int y2=5;
+		int x3=3;
+		int y3=3;
+		int x4=10;
+		int y4=10;
+		System.out.println("the cordinates of line 1 are::" +"("+x1+","+y1+")"+"and"+"("+x2+","+y2+")");
+		double Lengthline=Math.sqrt(((x2-x1) * (x2-x1)) + ((y2-y1) * (y2-y1))); 
+		System.out.println("Length of a line::"+Lengthline);
+		System.out.println("the cordinates of line 2 are::"+"("+x3+","+y3+")"+"and"+"("+x4+","+y4+")");
+		double Lengthline1=Math.sqrt(((x4-x3) * (x4-x3)) + ((y4-y3) * (y4-y3)));
+		System.out.println("Length of a line::"+Lengthline1);
 	}
 
 }
